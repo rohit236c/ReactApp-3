1aa35de33e73674b7c4ae80b8d459c991bb79930
rohit236c <rohit236c@gmail.com>
2019-05-29 13:05:01 +0530

    form submit added
    Signed-off-by: rohit236c <rohit236c@gmail.com>

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,25 +2,39 @@ import React from 'react';
 
 
 class SearchBar extends React.Component{
+    //initialised  in constructor
+    state = {term : ''}
+    
+    // a event handler...
+    // onInputChange (event){
+    //   console.log(event.target.value) 
+    // }
+    onFormSubmit=(event)=>{
 
-    onInputChange (event){
-      console.log(event.target.value) 
+        event.preventDefault();
+        console.log(this.state.term)
+
     }
-
-
-
+    
+    
     render (){
-       return (
-        <div className="ui segment">
-            <form className="ui form">
-                <div className="field">
-                    <label>Image Search</label>
-                <input type ="text"placeholder="search" onChange = {this.onInputChange} />
-                </div>
+        return (
+            <div className="ui segment">
+            <form  onSubmit={this.onFormSubmit}  className="ui form">
+            <div className="field">
+            <label>Image Search</label>
+            <input 
+            type ="text"
+            placeholder="search" 
+            onChange = {ev => this.setState({term : ev.target.value.toUpperCase()}) }
+            value={ this.state.term } 
+            />
+            </div>
             </form>
-        </div>
-       ) 
+            </div>
+            ) 
+        }
     }
-}
-
-export default SearchBar ;
+    
+    export default SearchBar ;
+    
